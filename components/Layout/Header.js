430e38af43ed871706928499852d323ca1c1fc46
shpilson <kovalenko.jora@gmail.com>
2022-11-13 20:00:12 -0300

    homepage ready

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -42,7 +42,7 @@ const Header = () => {
                   : " text-black-500 hover:text-orange-500 a")
               }
             >
-              О нас
+              Главная
             </LinkScroll>
             <LinkScroll
               activeClass="active"
@@ -139,7 +139,7 @@ const Header = () => {
                   d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                 />
               </svg>
-              О нас
+              Главная
             </LinkScroll>
             <LinkScroll
               activeClass="active"
