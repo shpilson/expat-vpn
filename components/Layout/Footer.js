f92625f9eadf901a8e69ea803f86421a2c850838
shpilson <kovalenko.jora@gmail.com>
2022-11-13 20:25:11 -0300

    fix

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -3,7 +3,7 @@ import LogoVPN from "../../public/assets/Logo.svg";
 import Telegram from "../../public/assets/Icon/telegram.svg";
 const Footer = () => {
   return (
-    <div className="bg-white-300 pt-44 pb-24">
+    <div className="bg-white-300 pt-64 pb-14">
       <div className="max-w-screen-xl w-full mx-auto px-6 sm:px-8 lg:px-16 grid grid-rows-3 sm:grid-rows-1 grid-flow-row sm:grid-flow-col grid-cols-3 sm:grid-cols-12 gap-4">
         <div className="sm:col-span-4 col-start-1 col-end-4 sm:col-end-5 flex flex-col items-start ">
           <LogoVPN className="h-8 w-auto mb-6" />
