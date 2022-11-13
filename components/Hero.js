7cd7d622cfc88b459bd34c25332f4dae04ebb47d
shpilson <kovalenko.jora@gmail.com>
2022-11-13 20:28:33 -0300

    fix

--- components/Hero.js
+++ components/Hero.js
@@ -33,7 +33,7 @@ const Hero = ({
     >
       <ScrollAnimationWrapper>
         <motion.div
-          className="grid grid-flow-row sm:grid-flow-col grid-rows-2 md:grid-rows-1 sm:grid-cols-2 gap-8 py-6 sm:py-16"
+          className="grid grid-flow-row sm:grid-flow-col grid-rows-1 md:grid-rows-1 sm:grid-cols-2 gap-8 py-6 sm:py-16"
           variants={scrollAnimation}>
           <div className=" flex flex-col justify-center items-start row-start-2 sm:row-start-1">
             <h1 className="text-3xl lg:text-4xl xl:text-5xl font-medium text-black-600 leading-normal">
